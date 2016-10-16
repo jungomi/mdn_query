f32cfcceb36b2d0bbd4f2eb5d32959007fb32b84
Michael Jungo <michaeljungo92@gmail.com>
2016-10-16 21:54:35 +0200

    Add Entry tests

---
+++ test/mdn_query/entry_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class MdnQueryEntryTest < Minitest::Test
+  def setup
+    @title = 'Title'
+    @description = 'Description'
+    @url = 'url'
+    @entry = ::MdnQuery::Entry.new(@title, @description, @url)
+  end
+
+  def test_to_s
+    expected_text = "Title\nDescription\nurl"
+    assert_equal @entry.to_s, expected_text
+  end
+
+  def test_open
+    spy = TestUtils::Spy.new
+    Launchy.stub(:open, spy.method) { @entry.open }
+    assert spy.called_once?
+    assert spy.called_with_args?(@url)
+  end
+
+  def test_content
+    content = 'Content'
+    spy = TestUtils::Spy.new(content)
+    @entry.stub(:retrieve, spy.method) do
+      refute spy.called?
+      @entry.content
+      assert spy.called_once?
+      assert_equal @entry.content, content
+      @entry.content
+    end
+    assert spy.called_once?
+    assert_equal @entry.content, content
+  end
+end
